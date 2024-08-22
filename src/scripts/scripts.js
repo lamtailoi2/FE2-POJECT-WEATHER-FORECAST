5c4d42e855e309bf3af959b2697d77a3b21e1484
Lamtailoi2 <lamtailoi11141@gmail.com>
2024-08-22 21:49:02 +0700

    feat: add openWeather api

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,7 +1,27 @@
 const searchInput = document.getElementById("city");
+const submitBtn = document.getElementById("submit");
 
-const handleSearch = (e) => {
-  console.log(e.target.value);
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const location = searchInput.value;
+  fetchData(location);
 };
 
-searchInput.addEventListener("input", handleSearch);
+const fetchData = async (location) => {
+  try {
+    const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&appid=e8710f95fd96060539ab0bcb1aa8ca60&lang=vi`;
+    const response = await fetch(URL);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! ${response.status}`);
+    }
+
+    const data = await response.json();
+    searchInput.value = "";
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+submitBtn.addEventListener("click", handleSubmit);
